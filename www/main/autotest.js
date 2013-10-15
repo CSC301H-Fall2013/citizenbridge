a698b1d2213b4b65aae66763e00ca3748b8d8bd4
LeoUfimtsev <LeosLetterbox@gmail.com>
2013-10-15 11:12:49 -0400

    added auto tests.

---
+++ www/main/autotest.js
@@ -0,0 +1,34 @@
+
+//All test cases go inside this function. Things outside the function will not be accessible (weird thing about JS scope)
+$(document).ready(function(){
+
+
+   //▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇►  helper functions. 
+
+   succes = function(id) 
+   {	
+   			x = document.getElementById(id);
+   			x.innerHTML='success';
+			x.className='success';
+   }
+   failure = function(id)
+   {	
+   			x = document.getElementById(id);
+   			x.innerHTML='failure';
+			x.className='failure';
+   }
+
+   //▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇►  Test Case definitions. 
+	exampleTest = function() 
+	{
+		if (1 == 1) 
+			succes('t1')
+		else 
+			failure('t1')
+	}
+
+	//▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇►  Test cases. 
+	exampleTest();
+	exampleTest2();
+
+});
