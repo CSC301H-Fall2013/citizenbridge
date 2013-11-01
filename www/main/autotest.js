0796859b697474eadf14ee83784db745daab33cc
LeoUfimtsev <LeosLetterbox@gmail.com>
2013-10-31 23:01:49 -0400

    added pie chart to test cases

--- www/main/autotest.js
+++ www/main/autotest.js
@@ -5,24 +5,42 @@
 
 
 //-----------------------  helper functions. 
-succes = function(id) 
+
+var success_count = 0;
+succes = function(id,msg)   //msg par is optional. 
 {  
+		//update the word seen in result. 
          x = document.getElementById(id);
          x.innerHTML='success';
          x.className='success';
+
+         //append messege to test case data field. 
+	     y = document.getElementById(id + "_data");
+	     if (y != null)
+	    	 y.innerHTML = msg;
+         
+	     //update success count
+	     success_count++;
 };
+failure_count = 0;
 failure = function(id)
 {  
+		 //update result to failure. 
          x = document.getElementById(id);
          x.innerHTML='failure';
          x.className='failure';
+         
+         //increment count
+         failure_count++;
 };
 
+not_implemented_count = 0;
 failureNotImplemented = function(id)
 {
          x = document.getElementById(id);
          x.innerHTML='failure test case not implemented';
-         x.className='failure';
+         x.className='notImplemented';
+         not_implemented_count++;
 };
 
 
@@ -41,44 +59,61 @@ exampleTest = function()
 //-----------------------  Test if we are able to download the representative list. 
 t2_acquire_rep_list = function(data) 
 {
-  if (data != null) 
-     succes('t2');
-  else
+  if (data != null) {
+	  
+	 msg = "Representatives count: " +  data.results.length;
+     succes('t2',msg);
+  } else
 	 failure('t2');
 };
 
 t3_acquire_bill_list = function(data)
 {
 	
-  if (data != null)  //usually there are 300+ members. If there are less then there is a problem.
-     succes('t3');
+  if (data != null){  //usually there are 300+ members. If there are less then there is a problem.
+	  msg = "Bills count: " +  data.results.length;
+	  succes('t3',msg);
+  }
   else
 	 failure('t3');
 };
 
 
 
-t4_acquire_induvidual_rep = function() 
+t4_acquire_induvidual_rep = function(data) 
 {
-
-   //This function is only reached if the downloaded rep list is successfull. 
-   test_downloaded_rep = function (data) 
-   {
-      result = data.results[0];
-      given  = result.name.given;
-      if (given == "Mike")  //usually there are 300+ members. If there are less then there is a problem.
-           succes('t4')
-   }
-
-   //Call the API to get the list. 
-   ParlData.reps("78554", "all", test_downloaded_rep);
-
-   //Time out after 4 seconds. 
-   setTimeout(function () {
-      x = document.getElementById('t4').innerHTML
-      if (x != 'success')
-         failure('t4')
-   }, 7000)
+  if (data != null){ //usually there are 300+ members. If there are less then there is a problem.
+     
+	  result = data.results[0];
+	  given = result.name.given;
+	  imgUrl = "http://www.parl.gc.ca/Parlinfo/images/Picture.aspx?Item=" + result.image_id; 
+	  
+	  msg = given + " " + "<img src='" + imgUrl + "'height='42' width='32'>";
+	  
+	  succes('t4', msg);
+  }
+  else
+	 failure('t4');
+	
+	
+//   //This function is only reached if the downloaded rep list is successfull. 
+//   test_downloaded_rep = function (data) 
+//   {
+//      result = data.results[0];
+//      given  = result.name.given;
+//      if (given == "Mike")  //usually there are 300+ members. If there are less then there is a problem.
+//           succes('t4')
+//   }
+//
+//   //Call the API to get the list. 
+//   ParlData.reps("78554", "all", test_downloaded_rep);
+//
+//   //Time out after 4 seconds. 
+//   setTimeout(function () {
+//      x = document.getElementById('t4').innerHTML
+//      if (x != 'success')
+//         failure('t4')
+//   }, 7000)
 }
 
 
@@ -132,7 +167,7 @@ t9_traverse_rep_list = function ()
 exampleTest();
 t2_acquire_rep_list(JS_AllRepList);
 t3_acquire_bill_list(JS_AllBillList);
-t4_acquire_induvidual_rep();
+t4_acquire_induvidual_rep(JS_SingleRep);
 t5_acquire_induvidual_bill();
 t6_();
 t7_();
@@ -140,3 +175,22 @@ t8_();
 t9_traverse_rep_list();
 
 
+
+
+
+//<!--LEO: Pie char business  -->
+var pieData = [
+		{
+			value: success_count,
+			color: "#80ff00"
+		},
+		{
+			value : failure_count,
+			color : "#ff0000"
+		},
+		{
+			value : not_implemented_count, 
+			color : "#FF8000"
+		}
+	];
+var myPie = new Chart(document.getElementById("canvas").getContext("2d")).Pie(pieData);
--- www/main/autotest.php
+++ www/main/autotest.php
@@ -11,6 +11,9 @@ $php_AllRepList = json_decode(file_get_contents("http://api.parliamentdata.ca/re
 $php_AllBillList = json_decode(file_get_contents("http://api.parliamentdata.ca/bills/", 0, $ctx));
 
 
+$php_singleRep = json_decode(file_get_contents("http://api.parliamentdata.ca/representatives/78554/all", 0, $ctx));
+
+
 		
 //get data about a specific rep 
 // 		$json = file_get_contents("http://api.parliamentdata.ca/representatives/" . $rep . "/all");
@@ -26,14 +29,35 @@ $php_AllBillList = json_decode(file_get_contents("http://api.parliamentdata.ca/b
 		<script src="js/jquery-1.10.2.js"></script>
 		<link type="text/css" rel="stylesheet" href="autotest.css"/>
 
+		
+		<!-- Pie chart business -->
+		<script src="frameworks/html5charts/Chart.js"></script>
+		<meta name = "viewport" content = "initial-scale = 1, user-scalable = no">
+		<style>
+			canvas{
+			}
+		</style>
+		
 	</head>
 	<body>
+	
+		<!-- Pie chart business -->
+		<!-- TODO_minor put into a table if I have time -->
+		<span style="color: #FF8000" > Not Implemented </span> <br>
+		<span style="color: #80ff00" > Successes </span> <br>
+		<span style="color: #ff0000"> Failures </span> <br>
+		
+
+		
+		<canvas id="canvas" height="300" width="300"></canvas><br>
+	
 		
 	
 	
+	
 		<table border="1">
-			<tr> <th> Test cases </th>   <th> Result</th>  </tr>
-			<tr> <td> Example test </td> <td id="t1">In Progress </td>  </tr> <!-- Copy & paste for additional test cases -->
+			<tr> <th> Test cases </th>   <th> Result</th>  <th> Test case data </th> </tr>
+			<tr> <td> Example test </td> <td id="t1">In Progress </td> <td id="t1_data"></td> </tr> <!-- Copy & paste for additional test cases -->
 
 
 			<tr> <td> <b> (User Story 1) Load Rep list </b> (this takes time) <br> 
@@ -43,17 +67,25 @@ $php_AllBillList = json_decode(file_get_contents("http://api.parliamentdata.ca/b
 				Note, the test auto-fails if the list is not retrieved within <b>7 seconds</b> <br>
 				or if you are not connected to the internet during testing. <br>
 				These test could fail if the API is overloaded or is unresponsive, <br>
-				In this case sometimes some of the tests fail and others succeed </td> <td id="t2"> In Progress </td> </tr>
-
+				In this case sometimes some of the tests fail and others succeed </td> 
+				<td id="t2"> In Progress </td>
+				<td id="t2_data"></td> </tr>
 
 			<tr> <td> <b> (U.S 6) Load Bills list </b> (this takes time) <br>
 				This test tests whether we are able to retrieve the<br> 
 				list of bills from the parliament website. <br>
-				Details are the same as above </td> <td id="t3"> In Progress </td> </tr>
-
+				Details are the same as above </td> 
+				<td id="t3"> In Progress </td>
+				<td id="t3_data"></td> </tr>
+				
+				
 			<tr> <td> <b> (2) Load induvidual Representative </b> <br> 
 				Tests if we can aquire information about an induvidual<br>
-				representative from the API. Details are the same as above.  </td> <td id="t4"> In Progress </td> </tr>
+				representative from the API. Details are the same as above.</td> 
+				<td id="t4"> In Progress </td>
+				<td id="t4_data"></td>
+				
+				</tr>
 
 			<tr> <td> <b> (7) Load induvidual Bill </b> <br> 
 				Tests if we can aquire information about an Bill<br>
@@ -79,8 +111,10 @@ $php_AllBillList = json_decode(file_get_contents("http://api.parliamentdata.ca/b
 	<script type="text/javascript">
 
 	//Load the downloaded list of reps into a global variable. Used by subsequent function calls
-	var JS_AllRepList = <?php echo json_encode($php_AllRepList)?>;
+	var JS_AllRepList  = <?php echo json_encode($php_AllRepList)?>;
 	var JS_AllBillList = <?php echo json_encode($php_AllBillList)?>;
+	var JS_SingleRep   = <?php echo json_encode($php_singleRep)?>;
+	
 	
 	</script>
 	
