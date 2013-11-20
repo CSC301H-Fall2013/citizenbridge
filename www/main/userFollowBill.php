c16e054f8e2408f63b285793f1df3be437f674d4
LeoUfimtsev <LeosLetterbox@gmail.com>
2013-11-20 14:39:56 -0500

    exteded follow bill functionaliy

--- www/main/userFollowBill.php
+++ www/main/userFollowBill.php
@@ -9,7 +9,6 @@ require("accountDatabase/common.php");
 if (empty($_POST) || empty($_SESSION)) 
 {
 	echo "Please log in before following a bill";
-			//todo add re-direct to main page here.
 };
 
 
@@ -18,7 +17,7 @@ $userEmail = $_SESSION['user']['email'];
 
 header("refresh:2;url=" . "http://" .  $_SERVER['SERVER_NAME'] . "/citizenbridge/www/main/bills.php?bill=" . $billToFollow);
 
-echo $billToFollow . " " .  $userEmail . "<br>";
+// echo $billToFollow . " " .  $userEmail . "<br>";
 
 //check if user+billid is already in the DB. 
 $sth = $db->prepare('SELECT bid FROM fbills WHERE bid = :bid AND email = :email');
@@ -37,14 +36,10 @@ if (isset($_POST['unfollow']))
 	// if true (I.e., if the bill-email pair is in the database) then remove it from the db
 	if($fetchResult)
 	{
-		echo "User will be removed from the DB";
+		echo "You no longer follow the bill";
 		//Remove bid and date into db.
 		$removeStatment = $db->prepare('DELETE FROM fbills WHERE bid = :bid AND email = :email');
 		$result = $removeStatment->execute(array(':bid' => $billToFollow, ':email' => $userEmail));
-	}
-	else 
-	{
-		echo "User+bill already not in the DB";
 	}
 }
 
@@ -54,14 +49,10 @@ else
 	//if false,  (I.e, if the bill is not present in our database) then insert the new bill into datbase.
 	if($fetchResult == false)
 	{
-		echo "User will be inserted into the DB";
+		echo "You're now following the bill";
 		//Insert bid and date into db.
 		$insertStatment = $db->prepare('INSERT INTO fbills (bid, email) VALUES (:bid, :email)');
 		$result = $insertStatment->execute(array(':bid' => $billToFollow, ':email' => $userEmail));
-	}
-	else 
-	{
-		echo "User+bill already in the DB";
 	}
 }
 
